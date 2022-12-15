6b3f736bbeeaa339b7495f70a78c0075e8f0e692
Kalcio <mmunozm@utem.cl>
2022-12-15 02:24:14 -0300

    Decoración pantalla Login

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:pokeapi_flutter/utils/colors_utils.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -11,6 +13,20 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                //Colores de la pantalla
+                colors: [
+              hexStringColor("0E8A57"),
+              hexStringColor("E5EBED"),
+              hexStringColor("FFFFFF")
+            ],
+                // Dirección del gradiente
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter)),
+      ),
+    );
   }
 }
